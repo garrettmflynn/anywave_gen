e7911d09be0fd9da0a3825d3c3c1b0931429d513
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2022-08-08 11:32:27 -0700

    Updated executable files and logging

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ const port = 5000;
 
 server.listen(port, () => {
     const url = 'http://localhost:5000'
-    console.log(`Server at ${url}`)
+    console.log(`Server at ${url}\n`)
 
     // opens the url in the default browser 
     open(
         url,
         // {app: 'firefox'}
     );
+
+    start()
 })
  
 // Configuiring simple express routes
@@ -45,30 +47,30 @@ function getDir() {
     } else {
         return path.join(require.main ? require.main.path : process.cwd());
     }
+}
 
 
 
-
+function start() {
 // Download AnyWave
 // grab the appropriate version for this hardware
-console.log('AnyWave installed! (todo)')
+console.log('AnyWave has been installed! (todo)')
 
 // Place the anywave_hed plugin (from Github) into the appropriate folder for this system
-console.log('anywave_hed installed! (todo) This will allow users to annotate BIDS datasets with HED tags.')
+console.log('anywave_hed has been installed! (todo) This will allow users to annotate BIDS datasets with HED tags.')
 
 // Place the anywave_stream plugin (from Github) into the appropriate folder for this system
-console.log('anywave_gen installed! (todo) This will stream your edits to the GEN server.')
+console.log('anywave_gen has been installed! (todo) This will stream your edits to the GEN server.')
 
 // configure python venv
-console.log('python venv configured! (todo)')
+console.log('python venv has been configured! (todo)')
 
 // open anywave programmatically in devmode
-console.log('AnyWave opened in developer mode! (todo)')
+console.log('AnyWave has been opened in developer mode! (todo)')
 
 // programmatically load the dataset into anywave and specify venv
-console.log('dataset loaded (todo)')
-console.log('venv specified (todo)')
-
-
+console.log('dataset has been loaded (todo)')
+console.log('venv has been specified (todo)')
+console.log('\nGEN is ready for your annotations!')
 
 }
