f1990306aa0b93d2957754a57dff6d9ef93e1bbf
Garrett <gflynn@usc.edu>
2022-11-14 10:50:17 -0800

    No anywave distribution pushed (too big...)

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # anywave_gen
- An AnyWave plugin for the Global EEG Norms (GEN) project
- 
+An AnyWave distribution package for the Global EEG Norms (GEN) project
+
+## Future Work
+- Finish the todo items noted in the comments of [index.js](./index.js)
+    - Test installation of AnyWave
+    - Automatically configure a Python venv
+    - Open AnyWave automatically in developer mode
+    - Specify the correct venv
+    - Load the appropriate dataset from a remote NAS
+    - Convert annotations to the BIDS format using [anywave_hed](https://github.com/garrettmflynn/anywave_hed)
+    - Send annotations to the remote NAS when AnyWave is closed
+- Package the latest version of AnyWave for Mac and Windows to be distributed with the executable files.
+- Update the [base html page](./views/index.html) with complete instructions that are specific to the packaged GEN project.
+
  ## Notes
  `xdg-open` must be distributed with the executable files to allow the browser to be automatically opened.
 
--- index.js
+++ index.js
@@ -6,6 +6,10 @@
 }());
 
 // Setting up our app requirements
+// anywave installation
+const extractDmg = require("extract-dmg");
+const os = require('os')
+const { exec } = require("child_process");
 
 const open = require('open');
 const express = require('express');
@@ -19,13 +23,15 @@ const port = 5000;
 
 server.listen(port, () => {
     const url = 'http://localhost:5000'
-    console.log(`Server at ${url}`)
+    console.log(`Server at ${url}\n`)
 
     // opens the url in the default browser 
     open(
         url,
         // {app: 'firefox'}
     );
+
+    start()
 })
  
 // Configuiring simple express routes
@@ -45,30 +51,79 @@ function getDir() {
     } else {
         return path.join(require.main ? require.main.path : process.cwd());
     }
+}
 
 
+function installAnywave () {
+
+    const res = new Promise(async (resolve, reject) => {
+    console.log('Starting to install AnyWave...')
+    // grab the appropriate version of Anywave for this hardware
+    const platform = os.platform()
+    switch (platform) {
+        case 'win32':
+            // http://meg.univ-amu.fr/AnyWave/AnyWave_win64.zip
+            fs.createReadStream('./anywave/AnyWave_win64.zip').pipe(unzip.Extract({ path: './anywave/extracted' }));
+
+            exec("./anywave/extracted/mysetup.exe", (error, stdout, stderr) => {
+                if (error) {
+                    reject(`error: ${error.message}`)
+                    return;
+                }
+                if (stderr) {
+                    reject(`stderr: ${stderr}`)
+                    return;
+                }
+                console.log(`${stdout}`);
+                resolve(true)
+            });
+            // run setup.exe
+        case 'linux':
+            console.error('[anywave_gen] Error: Linux OS not supported...')
+            // chmod 755 install_AnyWave.run
+            // sudo ./install_AnyWave.run
+        case 'darwin':
+            // https://meg.univ-amu.fr/AnyWave/AnyWave.dmg
+            await extractDmg("./anywave/AnyWave.dmg", "./anywave/extracted"); // Extract and get contents
+            exec("installer -pkg ./anywave/extracted/Anywave.pkg -target CurrentUserHomeDirectory", (error, stdout, stderr) => {
+                if (error) {
+                    reject(`error: ${error.message}`)
+                    return;
+                }
+                if (stderr) {
+                    reject(`stderr: ${stderr}`)
+                    return;
+                }
+                console.log(`${stdout}`);
+                resolve(true)
+            });
+    }
+})
+console.log(`Anywave has been installed!`)
+return res
+}
+
+async function start() {
 
 
-// Download AnyWave
-// grab the appropriate version for this hardware
-console.log('AnyWave installed! (todo)')
+    const result = await installAnywave()
 
-// Place the anywave_hed plugin (from Github) into the appropriate folder for this system
-console.log('anywave_hed installed! (todo) This will allow users to annotate BIDS datasets with HED tags.')
+    // Place the anywave_hed plugin (from Github) into the appropriate folder for this system
+    console.log('anywave_hed has been installed! (todo) This will allow users to annotate BIDS datasets with HED tags.')
 
-// Place the anywave_stream plugin (from Github) into the appropriate folder for this system
-console.log('anywave_gen installed! (todo) This will stream your edits to the GEN server.')
+    // configure python venv
+    console.log('python venv has been configured! (todo)')
 
-// configure python venv
-console.log('python venv configured! (todo)')
+    // open anywave programmatically in devmode
+    console.log('AnyWave has been opened in developer mode! (todo)')
 
-// open anywave programmatically in devmode
-console.log('AnyWave opened in developer mode! (todo)')
+    // programmatically load the dataset into anywave and specify venv
+    console.log('dataset has been loaded (todo)')
+    console.log('venv has been specified (todo)')
+    console.log('\nGEN is ready for your annotations!')
 
-// programmatically load the dataset into anywave and specify venv
-console.log('dataset loaded (todo)')
-console.log('venv specified (todo)')
-
+    // press a button on the UI to send data to our NAS!
+    console.log('Sending data to the GEN NAS!')
 
 
 }
